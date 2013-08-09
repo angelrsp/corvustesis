c08326950311ca70c09a75b3e005adea4298e109
fensefernando@gmail.com <fensefernando@gmail.com@5d685968-dcd6-f8d7-f702-b331c0723f45>
2013-08-09 04:38:13 +0000

    f

--- trunk/erpsistema/ejbModule/ec/edu/uce/erpmunicipal/contabilidad/bsl/JournalService.java
+++ trunk/erpsistema/ejbModule/ec/edu/uce/erpmunicipal/contabilidad/bsl/JournalService.java
@@ -1,5 +1,11 @@
 package ec.edu.uce.erpmunicipal.contabilidad.bsl;
 
+import java.util.List;
+
+import ec.edu.uce.erpmunicipal.contabilidad.orm.ConMovimientoDetalle;
+
 public interface JournalService {
+
+	void create(List<ConMovimientoDetalle> details);
 
 }
